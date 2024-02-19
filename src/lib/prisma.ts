51a6d3ab99d0ab0d52820456d6e50259f9faa882
Bruno Felix <brunofelixdesousa@hotmail.com>
2024-02-19 20:03:49 -0300

    docs: comentando lib prisma

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,6 +1,11 @@
+// Importa o módulo 'env' do diretório raiz com o alias '@/env'
 import { env } from '@/env'
+// Importa a classe 'PrismaClient' do pacote '@prisma/client'
 import { PrismaClient } from '@prisma/client'
 
+// Configura e exporta uma instância do PrismaClient
 export const prisma = new PrismaClient({
+  // Configuração do log: Se o ambiente for 'dev', registra apenas eventos de tipo 'query' (consultas ao banco de dados),
+  // caso contrário, nenhum log será registrado.
   log: env.NODE_ENV === 'dev' ? ['query'] : [],
 })
