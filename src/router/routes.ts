eee702db5082bc3fa22015bf57d01ca9b00af269
Bruno Felix <brunofelixdesousa@hotmail.com>
2024-02-19 23:15:04 -0300

    docs: comentando routes

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -1,7 +1,12 @@
+// Importa as funções listarCargos e salvarCargo do módulo cargoController
 import { listarCargos, salvarCargo } from '@/controllers/cargoController'
+// Importa o tipo FastifyInstance para representar a instância do Fastify
 import { FastifyInstance } from 'fastify'
 
+// Define a função appRoutes, responsável por configurar as rotas no aplicativo Fastify
 export async function appRoutes(app: FastifyInstance) {
+  // Configura a rota POST '/cargo', associando-a à função salvarCargo do controller
   app.post('/cargo', salvarCargo)
+  // Configura a rota GET '/cargo', associando-a à função listarCargos do controller
   app.get('/cargo', listarCargos)
 }
