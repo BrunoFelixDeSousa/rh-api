cd97583259fa024ad1565f0d90eeb12875a7f90b
Bruno Felix <brunofelixdesousa@hotmail.com>
2024-02-19 23:33:20 -0300

    docs: comentando app

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,11 @@
+// Importa a biblioteca Fastify para criar uma instância de servidor HTTP
 import fastify from 'fastify'
+
+// Importa as rotas configuradas no módulo routes
 import { appRoutes } from './router/routes'
 
+// Cria uma instância do servidor Fastify
 export const app = fastify()
 
+// Registra as rotas configuradas no aplicativo Fastify
 app.register(appRoutes)
