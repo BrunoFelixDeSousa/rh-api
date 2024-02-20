5c42d7c63e55558da7460ee8061ba424df43eda4
Bruno Felix <brunofelixdesousa@hotmail.com>
2024-02-19 23:35:17 -0300

    docs: comentando server

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,12 @@
 import { app } from './app'
 import { env } from './env'
 
+// Inicia o servidor e o faz escutar em um endereço e porta específicos
 app
   .listen({
-    host: '0.0.0.0',
-    port: env.PORT,
+    host: '0.0.0.0', // Define o host como '0.0.0.0' para escutar em todas as interfaces de rede
+    port: env.PORT, // Utiliza a porta especificada nas variáveis de ambiente
   })
   .then(() => {
-    console.log('🚀 HTTP Serve Running')
+    console.log('🚀 HTTP Serve Running') // Exibe uma mensagem indicando que o servidor está em execução
   })
