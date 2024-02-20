acdf45388bc8f26779c249c41e5df4c1cda95913
Bruno Felix <brunofelixdesousa@hotmail.com>
2024-02-19 23:09:49 -0300

    docs: comentando cargoRepository

--- src/repositories/cargoRepository.ts
+++ src/repositories/cargoRepository.ts
@@ -1,18 +1,28 @@
+// Importa a interface CargoRepositoryInterface, necessária para garantir a conformidade com o contrato definido
 import { CargoRepositoryInterface } from '@/interfaces/cargoRepositoryInterface'
+// Importa a instância prisma, que é uma instância do PrismaClient configurada
 import { prisma } from '@/lib/prisma'
+// Importa tipos relacionados a cargos e operações no Prisma
 import { Cargo, Prisma } from '@prisma/client'
 
+// Implementa a classe CargoRepository que segue a interface CargoRepositoryInterface
 export class CargoRepository implements CargoRepositoryInterface {
+  // Implementação do método create definido pela interface
   async create(data: Prisma.CargoCreateInput) {
+    // Utiliza a instância prisma para criar um novo cargo com os dados fornecidos
     const cargo = await prisma.cargo.create({
       data,
     })
-
+    // Retorna o cargo criado
     return cargo
   }
 
+  // Implementação do método findAll definido pela interface
   async findAll(): Promise<Cargo[]> {
+    // Utiliza a instância prisma para encontrar todos os cargos existentes
     const cargos = await prisma.cargo.findMany()
+
+    // Retorna a lista de cargos encontrados
     return cargos
   }
 }
