76f2f4e11c3c3a8572e2739a1ca5c19b92be8bd7
Bruno Felix <brunofelixdesousa@hotmail.com>
2024-02-21 20:49:07 -0300

    docs: corrigindo comentários

--- src/interfaces/areaRepositoryInterface.ts
+++ src/interfaces/areaRepositoryInterface.ts
@@ -2,8 +2,8 @@ import { Area, Prisma } from '@prisma/client'
 
 // Define uma interface chamada AreaRepositoryInterface para representar um repositório de areas
 export interface AreaRepositoryInterface {
-  // Método para criar um novo nivel, recebendo dados do tipo Prisma.NivelCreateInput e retornando uma Promise com um objeto do tipo Nivel
+  // Método para criar uma nova area, recebendo dados do tipo Prisma.AreaCreateInput e retornando uma Promise com um objeto do tipo Area
   create(data: Prisma.AreaCreateInput): Promise<Area>
-  // Método para encontrar todos os niveis, retornando uma Promise com uma lista de objetos do tipo Nivel
+  // Método para encontrar todos as areas, retornando uma Promise com uma lista de objetos do tipo Area
   findAll(): Promise<Area[]>
 }
